bfe568692ccbe2421e546288f816a84badd784a5
Deep Sandhya <deepsandhya@Deeps-MacBook-Air.local>
2025-08-14 14:59:10 +0530

    separte package for ios

---
+++ videoFramework/src/iosMain/kotlin/dev/reprator/video/platform/impl/VideoPlayerImplIos.kt
@@ -0,0 +1,29 @@
+package dev.reprator.video.platform.impl
+
+import dev.reprator.video.modals.VideoSource
+import dev.reprator.video.platform.impl.VideoPlayer
+
+class VideoPlayerImplIos(): VideoPlayer {
+
+
+    override fun play() {
+        TODO()
+    }
+
+    override fun pause() {
+        TODO()
+    }
+
+    override fun isDisposed(): Boolean {
+        TODO()
+    }
+
+    override fun dispose() {
+        TODO()
+    }
+
+    override fun changeMedia(videoSource: VideoSource) {
+        TODO()
+    }
+
+}
