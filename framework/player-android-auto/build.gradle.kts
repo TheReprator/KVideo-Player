90d9387ca4434cbe163e7cedeb6ca7f53898523c
Vikram Singh <mr.vikram004@gmail.com>
2025-09-06 20:28:54 +0530

    automative complete (#10)

---
+++ framework/player-android-auto/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    val packageName = "dev.reprator.video"
+    namespace = packageName
+    compileSdk =
+        libs.versions.android.compileSdk
+            .get()
+            .toInt()
+
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_24
+        targetCompatibility = JavaVersion.VERSION_24
+    }
+
+    useLibrary("android.car")
+}
+
+kotlin {
+    jvmToolchain(24)
+}
+
+dependencies {
+    implementation(projects.framework.kmpPlayer)
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.bom.material3)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.foundation)
+
+    implementation(libs.android.media3.ui.compose)
+}
