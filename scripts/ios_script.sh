d1d47833ee4a93cd3c5f3432dc73e6e00693d216
Reprator <mr.vikram004@gmail.com>
2025-09-10 22:34:02 +0530

    sample moved to apple folder

--- scripts/ios_script.sh
+++ scripts/ios_script.sh
@@ -15,7 +15,7 @@ if [ $? -eq 0 ]; then
     open -a Simulator
 
     echo "🍎 Opening iOS project in Xcode..."
-    open sample/ios/iosApp/iosApp.xcodeproj
+    open sample/apple/iosApp/iosApp.xcodeproj
 
     echo ""
     echo "🎯 NEXT STEPS IN XCODE:"
